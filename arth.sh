d0b9adac9d073bea2fb1ea3ab771afd7f3fb641f
madhuri0301 <harshupatil0301@gmail.com>
2021-05-09 23:47:38 +0530

    Third Step Completed

--- arth.sh
+++ arth.sh
@@ -5,3 +5,7 @@ echo "Welcome to the arithmatic"
 read -p "Enter the First Number :" a
 read -p "Enter the Second Number :" b
 read -p "Enter the Third Number :" c
+
+sum=$(echo $a $b $c | awk '{print $1+$2*$3}')
+
+echo $sum
