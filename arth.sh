0c4a289b4f33b0488aca889350501a8da4617434
madhuri0301 <harshupatil0301@gmail.com>
2021-05-11 18:50:50 +0530

    Sevent Step Is Completed

--- arth.sh
+++ arth.sh
@@ -19,4 +19,7 @@ dict['sum3']=$sum3
 sum4=$(echo $a $b $c | awk '{print $1%$2+$3}')
 dict['sum4']=$sum4
 
-echo ${dict[@]}
+echo "Dictionary Value are :${dict[@]}"
+
+arr=(${dict[@]})
+echo "Value Of Array Are :${arr[@]}"
