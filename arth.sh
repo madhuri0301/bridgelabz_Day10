93082d84c15c91f9ae9c3af3626991a5aa3ddc66
madhuri0301 <harshupatil0301@gmail.com>
2021-05-11 18:42:26 +0530

    Sixth Step Is Done

--- arth.sh
+++ arth.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+declare -A dict
 echo "Welcome to the arithmatic"
 
 read -p "Enter the First Number :" a
@@ -7,11 +8,15 @@ read -p "Enter the Second Number :" b
 read -p "Enter the Third Number :" c
 
 sum=$(echo $a $b $c | awk '{print $1+$2*$3}')
+dict['sum']=$sum
 
 sum2=$(echo $a $b $c | awk '{print $1*$2+$3}')
+dict['sum2']=$sum2
 
 sum3=$(echo $a $b $c | awk '{print $1+$2/$3}')
+dict['sum3']=$sum3
 
 sum4=$(echo $a $b $c | awk '{print $1%$2+$3}')
+dict['sum4']=$sum4
 
-echo $sum $sum2 $sum3 $sum4
+echo ${dict[@]}
