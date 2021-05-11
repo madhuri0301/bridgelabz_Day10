6df50b7b5a0bcd1dd0ad8556c3011b5e6ece6eab
madhuri0301 <harshupatil0301@gmail.com>
2021-05-11 12:24:13 +0530

    sixth Step is Done

--- arth.sh
+++ arth.sh
@@ -12,5 +12,6 @@ sum2=$(echo $a $b $c | awk '{print $1*$2+$3}')
 
 sum3=$(echo $a $b $c | awk '{print $1+$2/$3}')
 
+sum4=$(echo $a $b $c | awk '{print $1%$2+$3}')
 
-echo $sum $sum2 $sum3
+echo $sum $sum2 $sum3 $sum4
